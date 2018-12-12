7e72fee33e0d0c9b5b91aa759b3f03c7f0645ca4
sergiocorato <sergiocorato@gmail.com>
2018-12-12 12:02:46 +0100

    [FIX] latin for Descrizione invoice line and RiferimentoNormativo tax

--- l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
@@ -585,7 +585,8 @@ class WizardExportFatturapa(models.TransientModel):
                 # see https://tinyurl.com/ycem923t
                 # and '&#10;' would not be correctly visualized anyway
                 # (for example firefox replaces '&#10;' with space
-                Descrizione=line.name.replace('\n', ' '),
+                Descrizione=line.name.replace('\n', ' ').encode(
+                    'latin', 'ignore').decode('latin'),
                 PrezzoUnitario=('%.' + str(
                     price_precision
                 ) + 'f') % prezzo_unitario,
@@ -648,7 +649,8 @@ class WizardExportFatturapa(models.TransientModel):
                 if not tax.law_reference:
                     raise UserError(
                         _("No 'law reference' field for tax %s") % tax.name)
-                riepilogo.RiferimentoNormativo = tax.law_reference
+                riepilogo.RiferimentoNormativo = tax.law_reference.encode(
+                    'latin', 'ignore').decode('latin')
             if tax.payability:
                 riepilogo.EsigibilitaIVA = tax.payability
             # TODO
