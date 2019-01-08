43eda4c4539ab165ba3839f96082cef24cddbdbf
eLBati <elbaddy@gmail.com>
2019-01-08 10:40:09 +0100

    "mancata consegna" is the correct notification for invoices sent to individual without PEC or codice destinatario, so it sould not be an error. Added tests
    Also "avvenuta trasmissione della fattura con impossibilità di recapito" notification must be handled and status tracked

--- l10n_it_fatturapa_pec/__openerp__.py
+++ l10n_it_fatturapa_pec/__openerp__.py
@@ -2,11 +2,20 @@
 # Author(s): Andrea Colangelo (andreacolangelo@openforce.it)
 # Copyright 2018 Openforce Srls Unipersonale (www.openforce.it)
 # Copyright 2018 Sergio Corato (https://efatto.it)
+<<<<<<< HEAD:l10n_it_fatturapa_pec/__openerp__.py
 # Copyright 2018 Lorenzo Battistini <https://github.com/eLBati>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Italian Localization - FatturaPA - Emission - PEC Support',
     'version': '8.0.1.1.0',
+=======
+# Copyright 2018-2019 Lorenzo Battistini <https://github.com/eLBati>
+# Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Italian Localization - Fattura elettronica - Supporto PEC',
+    'version': '10.0.1.3.0',
+>>>>>>> 4c3f560c... "mancata consegna" is the correct notification for invoices sent to individual without PEC or codice destinatario, so it sould not be an error.:l10n_it_fatturapa_pec/__manifest__.py
     'category': 'Localization/Italy',
     'summary': 'Send electronic invoices via PEC',
     'author': 'Openforce Srls Unipersonale, Odoo Community Association (OCA)',
