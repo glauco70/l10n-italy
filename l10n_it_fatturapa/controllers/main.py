e1e3ca198bfcf2728654a2995fb0968abb15b721
Enrico Ganzaroli <enrico.gz@gmail.com>
2019-01-21 20:58:41 +0100

    [FIX] preview fatturapa

--- l10n_it_fatturapa/controllers/main.py
+++ l10n_it_fatturapa/controllers/main.py
@@ -17,7 +17,7 @@ def html_to_pdf(html):
     html_tmp_file_fd, html_tmp_file_path = tempfile.mkstemp(suffix='.html')
     with closing(os.fdopen(html_tmp_file_fd, 'wb')) as html_tmp_file:
         html_tmp_file.write(html)
-    pdf_tmp_file_fd, pdf_tmp_file_path = tempfile.mkstemp(suffix='.pdf', prefix='report.tmp.')
+    pdf_tmp_file_fd, pdf_tmp_file_path = tempfile.mkstemp(suffix='.pdf')
     os.close(pdf_tmp_file_fd)
     c = ["wkhtmltopdf", html_tmp_file_path, pdf_tmp_file_path]
     subprocess.call(c)
@@ -31,7 +31,8 @@ class FatturaElettronicaController(openerpweb.Controller):
     def preview(self, req, attachment_id, **kw):
         try:
             fix_session(req)
-            html = req.session.model('ir.attachment').get_fattura_elettronica_preview([int(attachment_id)])
+            a = req.session.model('ir.attachment')
+            html = a.get_fattura_elettronica_preview([int(attachment_id)])
             pdf = html_to_pdf(html)
             pdfhttpheaders = [
                 ('Content-Type', 'application/pdf'),
