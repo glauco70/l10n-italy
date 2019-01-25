1a49971c64ee2a0ceaa0847d1a5643d9bf5cbe41
sergiocorato <sergiocorato@gmail.com>
2019-01-25 18:16:51 +0100

    [FIX] use amount_currency if present

--- l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out/wizard/wizard_export_fatturapa.py
@@ -828,7 +828,8 @@ class WizardExportFatturapa(orm.TransientModel):
             for move_line_id in payment_line_ids:
                 move_line = move_line_pool.browse(
                     cr, uid, move_line_id, context=context)
-                ImportoPagamento = '%.2f' % move_line.debit
+                ImportoPagamento = '%.2f' % (
+                    move_line.amount_currency or move_line.debit)
                 DettaglioPagamento = DettaglioPagamentoType(
                     ModalitaPagamento=(
                         invoice.payment_term.fatturapa_pm_id.code),
