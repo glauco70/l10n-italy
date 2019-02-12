2b8b62b0e03d276f6362dd460dfb7ae853a656d9
eLBati <elbaddy@gmail.com>
2019-02-12 17:50:45 +0100

    Rename to l10n_it_invoices_data_communication Improve meta data ADD setup dir
    IMP l10n_it_invoices_data_communication using english and making terms translatable
    Increase version
    REF some methods

--- l10n_it_invoices_data_communication/models/account_invoice.py
+++ l10n_it_invoices_data_communication/models/account_invoice.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 
-from odoo import api, fields, models, _
+from odoo import fields, models, _
 from odoo.exceptions import ValidationError
 
 
-class account_invoice(models.Model):
+class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     comunicazione_dati_iva_escludi = fields.Boolean(
-        string='Escludi dalla dichiarazione IVA', default=False)
+        string='Exclude from invoices communication')
 
     def _compute_taxes_in_company_currency(self, vals):
         try:
@@ -84,12 +84,12 @@ class account_invoice(models.Model):
             val = {}
         if val['Aliquota'] == 0 and not val['Natura_id']:
             raise ValidationError(
-                _("Specificare la natura dell'esenzione per l'imposta: {}\
-                - Fattura {}"
-                  ).format(tax.name, self.number or False))
+                _(
+                    "Please specify exemption kind for tax: {} - Invoice {}"
+                ).format(tax.name, self.number or False))
         if not val['EsigibilitaIVA']:
             raise ValidationError(
-                _("Specificare l'esigibilità IVA per l'imposta: {}\
-                - Fattura {}"
-                  ).format(tax.name, self.number or False))
+                _(
+                    "Please specify VAT payability for tax: {} - Invoice {}"
+                ).format(tax.name, self.number or False))
         return val
