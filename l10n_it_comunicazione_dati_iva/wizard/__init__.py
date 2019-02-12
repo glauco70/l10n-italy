6db0dca3b97d668a1c15946582e15981b419f54b
eLBati <elbaddy@gmail.com>
2019-02-12 17:50:45 +0100

    Rename to l10n_it_invoices_data_communication Improve meta data ADD setup dir
    IMP l10n_it_invoices_data_communication using english and making terms translatable
    Increase version
    REF some methods

--- l10n_it_comunicazione_dati_iva/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-from . import export_file
-from . import compute_fiscal_document_type
--- l10n_it_invoices_data_communication/wizard/__init__.py
+++ l10n_it_invoices_data_communication/wizard/__init__.py
@@ -3,4 +3,3 @@
 
 from . import export_file
 from . import compute_fiscal_document_type
-from . import split_big_communication
