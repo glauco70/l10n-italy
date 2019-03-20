4c8d084cb41035f1e1e1f9d5ae13c70cae4708f1
eLBati <elbaddy@gmail.com>
2019-03-20 12:29:04 +0100

    FIX l10n_it_fatturapa_out_ddt to make it compatible with delivery module.
    Otherwise
    
    ERROR: test_e_invoice_ddt (odoo.addons.l10n_it_fatturapa_out_ddt.tests.test_fatturapa_ddt.TestInvoiceDDT)
    Traceback (most recent call last):
    `   File "/home/travis/build/eLBati/l10n-italy/l10n_it_fatturapa_out_ddt/tests/test_fatturapa_ddt.py", line 58, in test_e_invoice_ddt
    `     (self.so1.picking_ids | self.so2.picking_ids).do_transfer()
    `   File "/home/travis/odoo-10.0/addons/delivery/models/stock_picking.py", line 113, in do_transfer
    `     self.ensure_one()
    `   File "/home/travis/odoo-10.0/odoo/models.py", line 4859, in ensure_one
    `     raise ValueError("Expected singleton: %s" % self)
    ` ValueError: Expected singleton: stock.picking(42, 43)

--- l10n_it_fatturapa_out_ddt/tests/test_fatturapa_ddt.py
+++ l10n_it_fatturapa_out_ddt/tests/test_fatturapa_ddt.py
@@ -55,7 +55,8 @@ class TestInvoiceDDT(FatturaPACommon):
         })
         self.so1.action_confirm()
         self.so2.action_confirm()
-        (self.so1.picking_ids | self.so2.picking_ids).do_transfer()
+        self.so1.picking_ids.do_transfer()
+        self.so2.picking_ids.do_transfer()
         self.env['ddt.from.pickings'].with_context({
             'active_ids': self.so1.picking_ids.ids
             }).create({}).create_ddt()
