cb3ab05d91b44bfbf3254a732594533d18b73a18
sergiocorato <sergiocorato@gmail.com>
2019-06-09 23:54:26 +0200

    [FIX] invoice data communication
    [FIX] invoice data communication warnings
    
    [FIX] invoice data communication view
    
    [FIX] invoice data communication view
    
    [FIX] invoice data communication __openerp__
    
    [FIX] invoice data communication desc_
    
    [FIX] invoice data communication ensure_one
    
    [FIX] invoice data communication pa views
    
    [FIX] invoice data communication pa readme
    
    [FIX] invoice data communication pa readme
    
    [FIX] invoice data communication readme
    
    [FIX] invoice data communication readme
    
    [FIX] invoice data communication ensure one
    
    [FIX] invoice data communication ensure one
    
    [FIX] invoice data communication ensure one
    
    [FIX] invoice data communication ensure one
    
    [FIX] invoice data communication get exchange rate
    
    [FIX] invoice data communication view
    
    [FIX] invoice data communication pa index
    
    [FIX] invoice data communication index
    
    [FIX] invoice data communication search date
    
    [FIX] invoice data communication depends
    
    [FIX] invoice data communication import
    
    [FIX] invoice data communication partner
    
    [FIX] invoice data communication partner
    
    [FIX] rename partner
    2
    
    [FIX] supplier invoice number

--- l10n_it_invoices_data_communication/wizard/compute_fiscal_document_type.py
+++ l10n_it_invoices_data_communication/wizard/compute_fiscal_document_type.py
@@ -27,6 +27,6 @@ class ComunicazioneDatiIvaRicalcoloTipoDocumentoFiscale(models.TransientModel):
                     fattura.fiscal_document_type_id =\
                         fattura._get_document_fiscal_type(
                             type=fattura.type, partner=fattura.partner_id,
-                            fiscal_position=fattura.fiscal_position_id,
+                            fiscal_position=fattura.fiscal_position,
                             journal=fattura.journal_id)[0] or False
             return {}
